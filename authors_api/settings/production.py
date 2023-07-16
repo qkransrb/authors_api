c34b556ffcbd4640ebfe4817145ebdcd50a9e89d
qkransrb <qkransrb90@gmail.com>
2023-07-16 16:18:44 +0900

    setup basic django project

---
+++ authors_api/settings/production.py
@@ -0,0 +1,7 @@
+from .base import *  # noqa
+from .base import env
+
+ADMINS = [("qkransrb90", "qkrasnrb90@gmail.com")]
+
+# TODO and dmoan names of the production server
+CSRF_TRUSTED_ORIGINS = [""]
